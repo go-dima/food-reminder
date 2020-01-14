856ff17060da63e6956ee939c435c7e0fcc8aba2
go-dima <dima.gonikman@gmail.com>
2020-01-14 14:54:18 +0000

    Add 5 secs delayed callback

--- telegram_food_bot.py
+++ telegram_food_bot.py
@@ -3,30 +3,39 @@
 from telegram.ext import Updater, InlineQueryHandler, CommandHandler
 import sanity_bot
 from utils import trycatch
+from datetime import datetime, timedelta
 
 
 def store_clients():
-    print("Storing data")
     with open(CLIENTS_DATA, "w") as data_file:
         data_file.write('\n'.join(clients))
+    print("Stored data:", clients)
+
+
+def callback_alarm(context):
+    context.bot.send_message(chat_id=context.job.context, text='Hi, This is a callback')
+
+
+def schedule(context, client):
+    context.bot.send_message(chat_id=client, text='A callback has been set!')
+    context.job_queue.run_daily(callback_alarm, context=client, days=(0, 1, 2, 3, 4, 5, 6), time=datetime.now() + timedelta(seconds=5))
 
 
 def subscribe(bot, context):
-    client = str(bot.message.chat.id)
-    print(client)
-    if client not in clients:
-        clients.append(client)
+    client_id = str(bot.message.chat.id)
+    if client_id not in clients:
+        clients.append(client_id)
         store_clients()
         bot.message.reply_text(f"Hello! Your'e new here!")
+        schedule(context, client_id)
     else:
         bot.message.reply_text(f"Welcome back =]")
 
 
 def unsubscribe(bot, context):
-    client = str(bot.message.chat.id)
-    print(client)
-    if client in clients:
-        clients.remove(client)
+    client_id = str(bot.message.chat.id)
+    if client_id in clients:
+        clients.remove(client_id)
         store_clients()
         bot.message.reply_text(f"Goodbye!")
     else:
